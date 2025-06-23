3a4fd10407b2a2e9201804c0e22b7ef7710c72af
Semenenia Vladislav <vladsmnnya@gmail.com>
2025-06-23 11:16:55 +0300

    A navigation menu has been created for mobile devices

---
+++ frontend/src/components/MobileBottomNav.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import {
+    BottomNavigation, 
+    BottomNavigationAction, 
+    useMediaQuery, 
+} from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import PersonIcon from '@mui/icons-material/Person'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import TextsmsIcon from '@mui/icons-material/Textsms'
+import MenuIcon from '@mui/icons-material/Menu'
+import HomeIcon from '@mui/icons-material/Home'
+
+export default function MobileBottomNav() {
+    const [value, setValue] = useState(0)
+    const navigate = useNavigate()
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
+    if (!isMobile) return null 
+
+    return (
+        <BottomNavigation
+            value={value}
+            onChange={(event, value) => {setValue(newValue)}}
+            showLabels
+            sx={{
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                zIndex: 1100,
+                backgroundColor: 'background.paper',
+                borderTop: '1px solid rgba(0, 0, 0, 0.1)',
+                height: 66,
+                width: '100%'
+            }}>
+                <BottomNavigationAction
+                    label='Главная'
+                    icon={<HomeIcon fontSize='large' />}
+                    onClick={()=>navigate('/')}
+                    sx={{
+                        padding:0,
+                        height: 56,
+                        width: 86
+                    }}
+                />
+                <BottomNavigationAction
+                    label='Избранное'
+                    icon={<FavoriteIcon fontSize='large' />}
+                    onClick={()=>navigate('/')}
+                    sx={{
+                        padding:0,
+                        height: 56,
+                        width: 86
+                    }}
+                />
+                <BottomNavigationAction
+                    label='Мои объяв.'
+                    icon={<MenuIcon fontSize='large' />}
+                    onClick={()=>navigate('/')}
+                    sx={{
+                        padding:0,
+                        height: 56,
+                        width: 86
+                    }}
+                />
+                <BottomNavigationAction
+                    label='Сообщения'
+                    icon={<TextsmsIcon fontSize='large' />}
+                    onClick={()=>navigate('/')}
+                    sx={{
+                        padding:0,
+                        height: 56,
+                        width: 86
+                    }}
+                />
+                <BottomNavigationAction
+                    label='Профиль'
+                    icon={<PersonIcon fontSize='large' />}
+                    onClick={() => navigate('/')}
+                    sx={{
+                        padding:0,
+                        height: 56,
+                        width: 86
+                    }}
+                />
+        </BottomNavigation>
+    )
+}
