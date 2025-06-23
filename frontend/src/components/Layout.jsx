0fdcb7b97a2e8142539021b7117754b8bf6d2c3e
Semenenia Vladislav <vladsmnnya@gmail.com>
2025-06-23 11:13:54 +0300

    Craeted a Layout component

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,67 @@
+import { React, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+import { useTheme } from '@mui/material/styles'
+import { useMediaQuery, } from '@mui/material'
+import { 
+    AppBar, 
+    Toolbar, 
+    Box,
+} from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import AddIcon from '@mui/icons-material/Add'
+
+import SearchBar from './SearchBar'
+import MobileBottomNav from './MobileBottomNav'
+
+export default function Layout() {
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+    const navigate = useNavigate()  
+    const [searchTerm, setSearchTerm] = useState('')
+    const handleSearch = (term) => {setSearchTerm(term)}
+
+    if (!isMobile) {return (
+        null
+    )} else {return (
+        <>
+            <Box>
+                <AppBar 
+                    position='static'
+                    elevation={0}
+                    sx={{
+                        backgroundColor: 'transparent'
+                    }}
+                >
+                    <Toolbar sx={{padding:0}}>
+                        <Box>
+                            <SearchBar onSearch={handleSearch} />
+                        </Box>
+                        
+                        <Box sx={{marginLeft: 6}}>
+                            <IconButton aria-label='ShoppingCart' onClick={()=>navigate('/')}>
+                                <ShoppingCartIcon />
+                            </IconButton>
+
+                            <IconButton aria-label='Notifications' onClick={()=>navigate('/')}>
+                                <NotificationsIcon />
+                            </IconButton>
+
+                            <IconButton aria-label='CreateAListing' onClick={()=>navigate('/')}>
+                                <AddIcon />
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            
+            <Box>
+                <Outlet />
+            </Box>
+
+            <MobileBottomNav />
+        </>
+    )}
+}
