fc0e746f606c2990b0252b538a8b9bde64669244
Semenenia Vladislav <vladsmnnya@gmail.com>
2025-06-23 11:33:07 +0300

    Configuration of routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Layout from './components/Layout';
+
+import Home from './pages/Home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Layout /> } >
+          <Route index element={ <Home /> } />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
