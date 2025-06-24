2ab35891f717f871956e1d11929a9c382231f094
Semenenia Vladislav <vladsmnnya@gmail.com>
2025-06-24 13:00:08 +0300

    Edited route status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Layout from './components/Layout/Layout'
+
+import Home from './pages/Home'
 
 export default function App() {
   return(
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Layout /> } >
+          <Route index element={ <Home /> } />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,10 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Layout from './components/Layout';
-
-import Home from './pages/Home'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={ <Layout /> } >
-          <Route index element={ <Home /> } />
-        </Route>
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 )
