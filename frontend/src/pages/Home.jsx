2a638ed0a3df0d58493bba6a8e16baf75edc9cb7
Semenenia Vladislav <vladsmnnya@gmail.com>
2025-06-24 13:10:32 +0300

    Changed imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-
-import { Box, Card, CardActions, CardContent, Button, Typography } from '@mui/material'
-import OutlinedCard from '../components/Card'
+import { Box, Typography } from '@mui/material'
+import OutlinedCard from '../components/Card/Card'
 
 export default function Home() {
     return (
         <>
             <Box>
-                <p style={{fontSize: 28, width: '100%', margin: 0}}>Все объявления</p>
-                <OutlinedCard></OutlinedCard>
-                <OutlinedCard></OutlinedCard>
-                <OutlinedCard></OutlinedCard>
-                <OutlinedCard></OutlinedCard>
-                <OutlinedCard></OutlinedCard>
-                <OutlinedCard></OutlinedCard>
+                <Typography variant='h5'>Все объявления</Typography>
+                <OutlinedCard />
+                <OutlinedCard />
+                <OutlinedCard />
+                <OutlinedCard />
+                <OutlinedCard />
+                <OutlinedCard />
             </Box>
         </>
     )
